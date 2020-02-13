fab89cad8ff69924d026e9603c9c24026ccd7d29
Paul <paulrommel87@gmail.com>
2020-02-13 09:48:35 +0300

    Initial Commit

---
+++ MapUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapUI
+//
+//  Created by Павел Попов on 13.02.2020.
+//  Copyright © 2020 Павел Попов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
