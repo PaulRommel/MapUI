ab92e74c72e60daa12f87828ebaea3ebf9fa925e
Paul <paulrommel87@gmail.com>
2020-02-14 10:20:55 +0300

    insert menu

--- MapUI/ContentView.swift
+++ MapUI/ContentView.swift
@@ -9,18 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showMenu = false
+    
     var body: some View {
         GeometryReader { geometry in
-            MainView()
+            ZStack(alignment: .leading) {
+                MainView(showMenu: self.$showMenu)
                 .frame(width: geometry.size.width, height: geometry.size.height)
+                .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                .disabled(self.showMenu ? true : false)
+                
+                if self.showMenu {
+                    MenuView()
+                    .frame(width: geometry.size.width/2)
+                    .transition(.move(edge: .leading))
+                    }
+            }
         }
     }
 }
 
 struct MainView: View {
+    
+    @Binding var showMenu: Bool
+    
     var body: some View {
         Button(action: {
-            print("Open the side menu")
+            withAnimation {
+                self.showMenu = true
+                
+            }
         }) {
             Text("Show Menu")
         }
