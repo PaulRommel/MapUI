c251d075c2a5556b786e2e6ed4a68ffb889462e6
Paul <paulrommel87@gmail.com>
2020-02-14 09:33:21 +0300

    Designing menu

---
+++ MapUI/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  MapUI
+//
+//  Created by Павел Попов on 14.02.2020.
+//  Copyright © 2020 Павел Попов. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
